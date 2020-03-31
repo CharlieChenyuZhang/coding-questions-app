642eaa619b48cfa068aaca9371831b2a1cb017a9
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2020-03-30 23:11:54 -0400

    more questions

--- src/main/java/com/codingquestions/app/QueueAndStack/DequeByThreeStacks.java
+++ src/main/java/com/codingquestions/app/QueueAndStack/DequeByThreeStacks.java
@@ -16,4 +16,74 @@ import java.util.ArrayDeque;
 // TIME:
 // SPACE:
 public class DequeByThreeStacks {
+
+    // NOTE: for stackLeft, stackRight and buffer, I am using
+    // set of APIs: offerFirst(ele); pollFirst();
+    private Deque<Integer> stackLeft;
+    private Deque<Integer> stackRight;
+    private Deque<Integer> buffer;
+
+    public DequeByThreeStacks() {
+        stackLeft = new ArrayDeque<Integer>();
+        stackRight = new ArrayDeque<Integer>();
+        buffer = new ArrayDeque<Integer>();
+    }
+
+    public void offerFirst(int element) {
+        stackLeft.offerFirst(element);
+    }
+
+    public void offerLast(int element) {
+        stackRight.offerFirst(element);
+    }
+
+    public Integer pollFirst() {
+        move(stackRight, stackLeft);
+        return stackLeft.isEmpty() ? null : stackLeft.pollFirst();
+    }
+
+    public Integer pollLast() {
+        move(stackLeft, stackRight);
+        return stackRight.isEmpty() ? null : stackRight.pollFirst();
+    }
+
+    public Integer peekFirst() {
+        move(stackRight, stackLeft);
+        return stackLeft.isEmpty() ? null : stackLeft.peekFirst();
+
+    }
+
+    public Integer peekLast() {
+        move(stackLeft, stackRight);
+        return stackRight.isEmpty() ? null : stackRight.peekFirst();
+    }
+
+    public int size() {
+        return stackLeft.size() + stackRight.size();
+    }
+
+    public boolean isEmpty() {
+        return stackLeft.isEmpty() && stackRight.isEmpty();
+    }
+
+    // when dest is empty, move half of ele from src to dest
+    private void move(Deque<Integer> src, Deque<Integer> dest) {
+        // move one all elements from src to dest
+        if (!dest.isEmpty()) {
+            return;
+        }
+
+        int half = src.size() / 2;
+
+        for (int i = 0; i < half; i++) {
+            buffer.offerFirst(src.pollFirst());
+        }
+
+        while (!src.isEmpty()) {
+            dest.offerFirst(src.pollFirst());
+        }
+        while (!buffer.isEmpty()) {
+            src.offerFirst(buffer.pollFirst());
+        }
+    }
 }
