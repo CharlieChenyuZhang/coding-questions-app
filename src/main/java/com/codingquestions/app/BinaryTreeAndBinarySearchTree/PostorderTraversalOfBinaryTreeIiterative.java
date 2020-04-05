04d80643e55f9e18c924dea13cdc63b4c30307fc
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2020-04-04 21:06:06 -0400

    more questions

--- src/main/java/com/codingquestions/app/BinaryTreeAndBinarySearchTree/PostorderTraversalOfBinaryTreeIiterative.java
+++ src/main/java/com/codingquestions/app/BinaryTreeAndBinarySearchTree/PostorderTraversalOfBinaryTreeIiterative.java
@@ -8,12 +8,49 @@ import java.util.ArrayList;
 
 // Problem:
 /**
- *
+ * Implement an iterative, post-order traversal of a given binary tree, return
+ * the list of keys of each node in the tree as it is post-order traversed.
+ * 
+ * Examples
+ * 
+ * 5
+ * 
+ * / \
+ * 
+ * 3 8
+ * 
+ * / \ \
+ * 
+ * 1 4 11
+ * 
+ * Post-order traversal is [1, 4, 3, 11, 8, 5]
+ * 
+ * Corner Cases
+ * 
+ * What if the given binary tree is null? Return an empty list in this case. How
+ * is the binary tree represented?
+ * 
+ * We use the level order traversal sequence with a special symbol "#" denoting
+ * the null node.
+ * 
+ * For Example:
+ * 
+ * The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+ * 
+ * 1
+ * 
+ * / \
+ * 
+ * 2 3
+ * 
+ * /
+ * 
+ * 4
  */
 
 // Solution:
-// TIME:
-// SPACE:
+// TIME: O(n)
+// SPACE: O(height) if we didn't count the space used to stroe the result
 public class PostorderTraversalOfBinaryTreeIiterative {
     public List<Integer> postOrder(TreeNode root) {
         List<Integer> result = new ArrayList<>();
