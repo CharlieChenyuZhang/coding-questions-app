afbc20e3887117b2fd1e55f2d12658e98c8a4714
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2020-04-13 22:16:23 -0400

    more questions

--- src/main/java/com/codingquestions/app/HashTableAndStringI/CommonNumbersOfTwoSortedArraysArrayVersion.java
+++ src/main/java/com/codingquestions/app/HashTableAndStringI/CommonNumbersOfTwoSortedArraysArrayVersion.java
@@ -1,5 +1,19 @@
 package com.codingquestions.app.HashTableAndStringI;
 
+/**
+ * Find all numbers that appear in both of two sorted arrays (the two arrays are
+ * all sorted in ascending order).
+ * 
+ * Assumptions
+ * 
+ * In each of the two sorted arrays, there could be duplicate numbers. Both two
+ * arrays are not null. Examples
+ * 
+ * A = {1, 1, 2, 2, 3}, B = {1, 1, 2, 5, 6}, common numbers are [1, 1, 2]
+ */
+
+// TIEM
+// SPACE:
 public class CommonNumbersOfTwoSortedArraysArrayVersion {
 
 }
