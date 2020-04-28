7deaa3ccde64dc23efa66eedab0e6991bd75ee40
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2020-04-27 22:49:54 -0400

    more questions

--- src/main/java/com/codingquestions/app/RecursionII/ReverseLinkedListInPairs.java
+++ src/main/java/com/codingquestions/app/RecursionII/ReverseLinkedListInPairs.java
@@ -16,5 +16,32 @@ import com.codingquestions.app.utils.ListNode;
 // TIME: O(n)
 // SPACE: O(n)
 public class ReverseLinkedListInPairs {
+    public ListNode reverseInPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = head.next;
+        head.next = reverseInPairs(head.next.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    // iterative
+    public ListNode reverseInPairsIterative(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+        while (cur.next != null && cur.next.next != null) {
+            ListNode next = cur.next;
+            ListNode nextnextnext = cur.next.next.next;
+            cur.next = cur.next.next;
+            cur.next.next = next;
+            next.next = nextnextnext;
+
+            cur = cur.next.next;
+        }
+        return dummyHead.next;
+    }
 
 }
