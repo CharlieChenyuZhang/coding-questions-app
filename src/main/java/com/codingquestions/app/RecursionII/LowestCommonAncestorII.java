d68623bbede582b6a547577d314187e6b57d05e0
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2020-04-28 21:59:06 -0400

    more questions

---
+++ src/main/java/com/codingquestions/app/RecursionII/LowestCommonAncestorII.java
@@ -0,0 +1,70 @@
+package com.codingquestions.app.RecursionII;
+
+import com.codingquestions.app.util.TreeNodeP;
+
+/**
+ * Given two nodes in a binary tree (with parent pointer available), find their
+ * lowest common ancestor.
+ * 
+ * Assumptions
+ * 
+ * There is parent pointer for the nodes in the binary tree
+ * 
+ * The given two nodes are not guaranteed to be in the binary tree
+ * 
+ * Examples
+ * 
+ * 5
+ * 
+ * / \
+ * 
+ * 9 12
+ * 
+ * / \ \
+ * 
+ * 2 3 14
+ * 
+ * The lowest common ancestor of 2 and 14 is 5
+ * 
+ * The lowest common ancestor of 2 and 9 is 9
+ * 
+ * The lowest common ancestor of 2 and 8 is null (8 is not in the tree)
+ */
+
+// TIME: O(height1 + height2)
+// SPACE: O(1)
+public class LowestCommonAncestorII {
+    public TreeNodeP lowestCommonAncestor(TreeNodeP one, TreeNodeP two) {
+        int l1 = length(one);
+        int l2 = length(two);
+
+        if (l1 < l2) {
+            return mergeNode(one, two, l2 - l1);
+        } else {
+            return mergeNode(two, one, l1 - l2);
+        }
+    }
+
+    private int length(TreeNodeP node) {
+        int lengthToRoot = 0;
+        while (node != null) {
+            node = node.parent;
+            lengthToRoot++;
+        }
+        return lengthToRoot;
+    }
+
+    private TreeNodeP mergeNode(TreeNodeP shorter, TreeNodeP longer, int diff) {
+        while (diff > 0) {
+            longer = longer.parent;
+            diff--;
+        }
+
+        while (longer != shorter) {
+            longer = longer.parent;
+            shorter = shorter.parent;
+        }
+
+        return longer;
+    }
+}
---
+++ src/main/java/com/codingquestions/app/RecursionII/LowestCommonAncestorIII.java
@@ -0,0 +1,38 @@
+package com.codingquestions.app.RecursionII;
+
+/**
+ * Given two nodes in a binary tree, find their lowest common ancestor (the
+ * given two nodes are not guaranteed to be in the binary tree).
+ * 
+ * Return null If any of the nodes is not in the tree.
+ * 
+ * Assumptions
+ * 
+ * There is no parent pointer for the nodes in the binary tree
+ * 
+ * The given two nodes are not guaranteed to be in the binary tree
+ * 
+ * Examples
+ * 
+ * 5
+ * 
+ * / \
+ * 
+ * 9 12
+ * 
+ * / \ \
+ * 
+ * 2 3 14
+ * 
+ * The lowest common ancestor of 2 and 14 is 5
+ * 
+ * The lowest common ancestor of 2 and 9 is 9
+ * 
+ * The lowest common ancestor of 2 and 8 is null (8 is not in the tree)
+ */
+
+// TIME:
+// SPACE:
+public class LowestCommonAncestorIII {
+
+}
