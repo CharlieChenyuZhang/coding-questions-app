de278ca05ae0420fee06392c3feed6698f4bc152
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2020-05-12 10:45:30 -0400

    more questions

---
+++ src/main/java/com/codingquestions/app/DynamicProgramming/LargestSubMatrixSum.java
@@ -0,0 +1,29 @@
+package com.codingquestions.app.DynamicProgramming;
+
+/**
+ * Given a matrix that contains integers, find the submatrix with the largest
+ * sum.
+ * 
+ * Return the sum of the submatrix.
+ * 
+ * Assumptions
+ * 
+ * The given matrix is not null and has size of M * N, where M >= 1 and N >= 1
+ * Examples
+ * 
+ * { {1, -2, -1, 4},
+ * 
+ * {1, -1, 1, 1},
+ * 
+ * {0, -1, -1, 1},
+ * 
+ * {0, 0, 1, 1} }
+ * 
+ * the largest submatrix sum is (-1) + 4 + 1 + 1 + (-1) + 1 + 1 + 1 = 7.
+ * 
+ * 
+ */
+
+public class LargestSubMatrixSum {
+
+}
