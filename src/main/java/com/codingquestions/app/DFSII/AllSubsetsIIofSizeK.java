85d44b4f98b79d26717313d29d9222fad30fed40
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2020-05-20 23:25:52 -0400

    more questions

---
+++ src/main/java/com/codingquestions/app/DFSII/AllSubsetsIIofSizeK.java
@@ -0,0 +1,33 @@
+package com.codingquestions.app.DFSII;
+
+/**
+ * Given a set of characters represented by a String, return a list containing
+ * all subsets of the characters whose size is K. Notice that each subset
+ * returned will be sorted for deduplication.
+ * 
+ * 
+ * 
+ * Assumptions
+ * 
+ * There could be duplicate characters in the original set.
+ * 
+ * ​
+ * 
+ * Examples
+ * 
+ * Set = "abc", K = 2, all the subsets are [“ab”, “ac”, “bc”].
+ * 
+ * Set = "abb", K = 2, all the subsets are [“ab”, “bb”].
+ * 
+ * Set = "abab", K = 2, all the subsets are [“aa”, “ab”, “bb”].
+ * 
+ * Set = "", K = 0, all the subsets are [""].
+ * 
+ * Set = "", K = 1, all the subsets are [].
+ * 
+ * Set = null, K = 0, all the subsets are [].
+ */
+
+public class AllSubsetsIIofSizeK {
+
+}
